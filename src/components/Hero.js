4286f6b4e5b21f56fddba71da29ff5e0a591bf84
christine <rickyshie0724@gmail.com>
2023-09-28 16:47:49 +0900

    Adjust Hamburger.js component's layout to make it responsive

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -8,14 +8,14 @@ function Hero() {
         <section className="hero-grid">
             <h1 className="hero-grid-items hero-title">Little Lemon</h1>
             <h2 className="hero-grid-items hero-subtitle">Chicago</h2>
-            <p className="hero-grid-items hero-description">Lorem ipsum dolor sit amet, consectetur adipiscing elit, 
+            {/* <p className="hero-grid-items hero-description">Lorem ipsum dolor sit amet, consectetur adipiscing elit, 
                sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
                Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris
                nisi ut aliquip ex ea commodo consequat.</p>
             <img className="hero-grid-items hero-image" src="/images/restaurant_food.jpg"></img>
             <div className="hero-grid-items reserve-button">
             <Button className="hero-grid-items reserve-button" label="Reserve a Table"></Button>
-            </div>
+            </div> */}
         </section>
     )
 }
