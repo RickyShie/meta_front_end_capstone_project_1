bffa304e4cf35da03356be5b872232e7a3e904a1
christine <rickyshie0724@gmail.com>
2023-09-29 21:15:26 +0900

    Adjust Hero.js component's layout

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -6,16 +6,18 @@ import Button from "./Button";
 function Hero() {
     return (
         <section className="hero-grid">
-            <h1 className="hero-grid-items hero-title">Little Lemon</h1>
-            {/* <h2 className="hero-grid-items hero-subtitle">Chicago</h2>
-            <p className="hero-grid-items hero-description">Lorem ipsum dolor sit amet, consectetur adipiscing elit, 
-               sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
-               Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris
-               nisi ut aliquip ex ea commodo consequat.</p>
-            <img className="hero-grid-items hero-image" src="/images/restaurant_food.jpg" alt="hero"></img>
-            <div className="hero-grid-items reserve-button">
-            <Button label="Reserve a Table"></Button>
-            </div> */}
+            <div className="hero-grid-items hero-texts">
+                <h1 className="hero-title">Little Lemon</h1>
+                <h2 className="hero-subtitle">Chicago</h2>
+                <p className="hero-description">Lorem ipsum dolor sit amet, consectetur adipiscing elit, 
+                sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+                Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris
+                nisi ut aliquip ex ea commodo consequat.</p>
+                <Button label="Reserve a Table"></Button>
+            </div>
+            <div className="hero-grid-items">
+                <img className="hero-image" src="/images/restaurant_food.jpg" alt="hero"></img>
+            </div>
         </section>
     )
 }
