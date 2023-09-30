b6da3efb16255308c13ee29081c955b82c5fb1d9
christine <rickyshie0724@gmail.com>
2023-09-30 17:08:14 +0900

    Adjust Special.js component's layout

--- src/components/Specials.js
+++ src/components/Specials.js
@@ -10,12 +10,14 @@ dishes.map(dish => {console.log(dish.description);})
 
 function Specials() {
     return (
-        <section className="specials-grid">
+        <React.Fragment>
             <h1 className="special-title">This week's specials!</h1>
-            {dishes.map(dish => {
-            return <Card key={dish.id} title={dish.title} price={dish.price} description={dish.description} imageName={dish.imageName}> 
-            </Card>})}
-        </section>
+            <section className="specials-grid">
+                {dishes.map(dish => {
+                return <Card key={dish.id} title={dish.title} price={dish.price} description={dish.description} imageName={dish.imageName}> 
+                </Card>})}
+            </section>
+        </React.Fragment>
     )
 }
 
